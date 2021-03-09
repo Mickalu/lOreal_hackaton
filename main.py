3e6b7990c16ac45ad87ed12f7f9dfd812d5efc41
Mickalu <45756345+Mickalu@users.noreply.github.com>
2021-03-09 19:40:49 +0100

    model with test file added

--- main.py
+++ main.py
@@ -20,22 +20,28 @@ array_col = ['skincare', 'hair', 'make-up', 'other']
 df_train = pd.read_csv("data/hackathon_loreal_train_set.csv")
 df_train = df_train.drop(['Unnamed: 0'], axis = 1)
 
+df_test = pd.read_csv("data/hackathon_loreal_test_set.csv")
+df_test = df_test.drop(['Unnamed: 0'], axis = 1)
 
 
 
 df_train["clean_content"] = df_train.text.apply(clean_text)
+df_test["clean_content"] = df_test.text.apply(clean_text)
 
 
 
 df_train_clean = df_train.drop(["text"], axis = 1)
-X = df_train['clean_content']
-Y = df_train.drop(['clean_content', 'index', 'text'], axis = 1)
+df_test_clean = df_test.drop(["text"], axis = 1)
 
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=4)
 
+x_train = df_train_clean["clean_content"]
+x_test = df_test_clean["clean_content"]
+
+y_train = df_train_clean.drop(["clean_content", "index"], axis = 1)
 
 cv=CountVectorizer()
 cv.fit(x_train)
+
 x_train_count = cv.fit_transform(x_train).astype('int32').toarray()
 x_test_count = cv.transform(x_test).astype('int32').toarray()
 
@@ -43,7 +49,7 @@ x_test_count = cv.transform(x_test).astype('int32').toarray()
 
 model = Sequential()
 
-model.add(Dense(128, input_shape=(70574,), activation='relu'))
+model.add(Dense(128, input_shape=(80736,), activation='relu'))
 model.add(Dense(4, activation='softmax'))
 
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics = [f1_m])
@@ -51,4 +57,4 @@ model.compile(optimizer='adam', loss='binary_crossentropy', metrics = [f1_m])
 model.fit(x_train_count, y_train, epochs=5)
 
 
-evaluate = model.evaluate(x_test_count, y_test)
+y_predicted = model.predict(x_test_count)
